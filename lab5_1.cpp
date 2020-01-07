902f8afb995a329b3e19ebc6ea5e91f00ddc843b
aomsin1822544 <pspsas333_111@hotmail.com>
2020-01-07 16:37:08 +0700

    v.1

--- lab5_1.cpp
+++ lab5_1.cpp
@@ -1,25 +1,46 @@
 #include<iostream>
 using namespace std;
 
+
 int main(){
 	int N;
+	
 	cout << "Enter the number of student: ";
 	cin >> N;
 	
-	string name[N];
-	int age[N];
-	cout << "Name of student = ";
-	cout << "Age of student = ";
+	string name[N]; 
 	
+	int age[N];
+	int i = 0;
+	while(i<N){
+    cout << "Name of student = [" << i+1 <<"]:";
+	cin >> name[i];
+    cout << "Age of student = [" << i+1 <<"]:";
+	cin >> age[i];
+	i++;
+	}
+	 
 	int key;
 	cout << "--------------------------------------\n";
 	cout << "Enter an age to search: ";
 	cin >> key;
 	cout << "--------------------------------------\n";
+	int g = 0;
+	while(g<N){
+		if(key==age[g]){
+			cout << name[g] <<endl;
+			
+			
+		
+		}1
+		g++; 
+	} 
+
+	 
 	
 
 	
-	cout << "--------------------------------------\n";
+	cout << "-----------------------------------\n";
 	
 	return 0;
 }
